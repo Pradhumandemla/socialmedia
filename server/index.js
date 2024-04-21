bbe16725a0c2a8ce30e4007b35988fb7e90a9965
Pradhuman Demla <pradhumnd33@gmail.com>
2024-04-22 04:12:59 +0530

    initials
    Signed-off-by: Pradhuman Demla <pradhumnd33@gmail.com>

--- server/index.js
+++ server/index.js
@@ -36,6 +36,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
 app.use("/public/assets", express.static(path.join(__dirname, "public/assets")));
 // app.use('/static', express.static('public'))
 // app.use(express.static(path.join(__dirname, 'public/assets')));
